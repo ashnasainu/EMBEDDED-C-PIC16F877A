1ef0df07f7f30f6cdad064343ce555874276f16a
Ashinasainu <ashnasainudeen.eee@gmail.com>
2025-07-14 13:00:24 +0530

    ADC variable resistor program modified lcd.c modified lcd.h modified

--- ADC_variable resistor/ADC_variable resistor.X/LCD.h
+++ ADC_variable resistor/ADC_variable resistor.X/LCD.h
@@ -1,9 +1,9 @@
+#define RS  PORTBbits.RB0   // Register Select Pin (instruction or data register))
+#define RW  PORTBbits.RB1   // Read/Write Pin 
+#define EN  PORTBbits.RB2   // Enable Pin
+
 void lcd_initialise(void);
 void lcd_data(unsigned char data);
 void lcd_command(unsigned char cmnd);
 void lcd_string(const unsigned char *str);
 void lcd_set_cursor(int a, int b);
-
-
-
-
