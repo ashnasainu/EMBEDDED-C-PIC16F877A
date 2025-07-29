1fa4959b3d6d2b3bb6e6f348f22307f02d407277
Ashinasainu <ashnasainudeen.eee@gmail.com>
2025-07-29 17:01:05 +0530

    changes added to DS1307_RTC main.c modified rtc.h modified rtc.c modified

--- DS1307 _RTC/DS1307 _RTC.X/main.c
+++ DS1307 _RTC/DS1307 _RTC.X/main.c
@@ -6,33 +6,25 @@
 
 void main(void)
 {
-    TRISC=0XFF;     // port c as input
-    TRISD = 0x00;   // Set all pins of PORTD as output (0 = output, 1 = input)
+    TRISC=0XFF;         // port c as input   , I2C pins setting as input (SDL , SDA))
+    TRISD = 0x00;       // Set all pins of PORTD as output (0 = output, 1 = input)
+    PORTD=0X00;
+    SSPADD=49;          // I2C speed ,setting I2C clock(slave = RTC)  to be 100 KHz , getting from data sheet of DS1307
+    SSPCON=0x28;        // CONTROL REGISTER 2 (I2C MODE)  
     
     lcd_initialise();
     lcd_clear();
     lcd_set_cursor(1,0);
     lcd_string("CLOCK:");
-    __delay_ms(2000);
-    
-    SSPCON=40;     // CONTROL REGISTER 2 (I2C MODE)  
-    SSPADD=0x3f;   // I2C speed ,setting I2C clock(slave = RTC)  to be 100 KHz , getting from data sheet of DS1307
-    unsigned char hour=' ';
-    char DATA[10]=" ";
-   
+    lcd_set_cursor(2,0);
+    lcd_string("CLOCK:");
+    DS1307_write(3,2,1,1,5,4,20);
+    __delay_ms(500);
+        
     while(1)
     {
-        rtc_write(1);            // Write 1 to the RTC
-        __delay_ms(200);           // Delay (optional)
-        
-        hour=rtc_read();  
-        bcd_to_ascii(0x45,DATA); // Read back the hour
-        lcd_set_cursor(2,6);
-        lcd_string("  ");        // Clear old data
-         lcd_set_cursor(2,6);
-        lcd_string(DATA);
-        __delay_ms(200); 
-       
+        DS1307_read(DS1307_address,0);           // Reading from the RTC
+        __delay_ms(20);                           // Delay (optional) 
     }  
     return;
 }
--- DS1307 _RTC/DS1307 _RTC.X/rtc.h
+++ DS1307 _RTC/DS1307 _RTC.X/rtc.h
@@ -1,7 +1,17 @@
-void rtc_write(int data);
+#define DS1307_address  0xD0                       // rtc macros for ds1307
+#define second          0x00
+#define minute          0x01
+#define hour            0x02
+#define day             0x03
+#define date            0x04
+#define month           0x05
+#define year            0x06
+#define control_reg     0x07
+
 void i2c_start();
 void i2c_stop();
 void i2c_write();
-int decimal_bcd();
-int rtc_read();
-void bcd_to_ascii(unsigned char bcd, unsigned char *ascii);
+void bcd_to_ascii(unsigned char value);
+char decimal_to_bcd(unsigned char value);
+void DS1307_write(char _second, char _minute, char _hour, char _day, char _date, char _month, char _year);
+void DS1307_read(char slave_address, char register_address);
